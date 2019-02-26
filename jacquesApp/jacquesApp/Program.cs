3a11e6e274cbb663c8bc38a4d2f09d8234a8585a
Jacques Rockell <30013256@student.toiohomai.ac.nz>
2019-02-27 10:44:48 +1300

    test

---
+++ jacquesApp/jacquesApp/Program.cs
@@ -0,0 +1,32 @@
+﻿/*fowrdslash star is for multi line*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jacquesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string name;
+
+
+
+
+            Console.WriteLine("Please enter your age: ");
+            Console.WriteLine("Pls entr name");
+        
+            age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(value: name+age);
+
+            Console.ReadLine();
+        }
+    }
+}
