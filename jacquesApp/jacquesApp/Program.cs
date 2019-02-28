c4f2154d3d958454e56190347de1f1492b1517ea
Jacques Rockell <30013256@student.toiohomai.ac.nz>
2019-02-28 13:40:19 +1300

    test 2

--- jacquesApp/jacquesApp/Program.cs
+++ jacquesApp/jacquesApp/Program.cs
@@ -13,20 +13,57 @@ namespace jacquesApp
     {
         static void Main(string[] args)
         {
-            int age;
-            string name;
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+            int pin = 123;
+            decimal savings = 100.00M;
 
 
 
+            Console.WriteLine("Please enter your firstname: ");
+            string firstname = Console.ReadLine();
 
-            Console.WriteLine("Please enter your age: ");
-            Console.WriteLine("Pls entr name");
-        
-            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your lastname: ");
+            string lastname = Console.ReadLine();
 
-            Console.WriteLine(value: name+age);
+            Console.WriteLine("Please enter your age");
+            int age = int.Parse(Console.ReadLine());
+
+
+            Console.Write (firstname + " " + lastname+" ");
+
+            Console.WriteLine("Enter pin");
+            pin = int.Parse (Console.ReadLine());
+
+            if (pin == 123 && age == 17 && firstname == "Jacques" && lastname == "Rockell")
+            {
+
+                Console.WriteLine("Unlocked");
+
+                Console.WriteLine("Enter withdraw ammount:");
+                decimal withdraw = decimal.Parse(Console.ReadLine());
+
+                Console.WriteLine(firstname + " " + lastname + " " + "Savings$" + savings+"-" + "$" + withdraw);
+
+                savings = savings - withdraw;
+
+                Console.WriteLine("Your new ballence is $" + savings);
+            }
+
+            else
+            {
+                Console.WriteLine("Locked");
+
+            }
+
+                
+
 
             Console.ReadLine();
+
+
         }
     }
 }
