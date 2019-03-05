96afa86367f8159919f2a32567282f6f44951eeb
Jacques Rockell <30013256@student.toiohomai.ac.nz>
2019-03-06 11:29:52 +1300

    2 app

--- jacquesApp/jacquesApp/Program.cs
+++ jacquesApp/jacquesApp/Program.cs
@@ -1,20 +1,130 @@
-﻿/*fowrdslash star is for multi line*/
-
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace jacquesApp
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-
             Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Enter 1 for Calculator or 2 for Bank");
+
+            int app = int.Parse(Console.ReadLine());
+
+
+
+            if (app == 1)
+            {
+                Console.Clear();
+                Calulator();
+
+
+            }
+
+            else if (app == 2)
+            {
+                Console.Clear();
+                Bank();
+
+            }
+
+        }
+
+
+
+        public static void Calulator()
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+
+
+
+            Console.WriteLine("Enter your calculation");
+
+            Double num1 = double.Parse(Console.ReadLine());
+
+            string calulation = Console.ReadLine();
+
+            Double num2 = double.Parse(Console.ReadLine());
+
+            double resalt = 0;
+
+            if (calulation == "+")
+            {
+
+                Add(num1, num2);
+                resalt = num1 + num2;
+
+
+            }
+
+
+            else if (calulation == "/")
+            {
+
+                resalt = num1 / num2;
+
+            }
+
+            else if (calulation == "-")
+            {
+                resalt = num1 - num2;
+
+
+            }
+
+
+            else if (calulation == "*")
+            {
+
+                resalt = num1 * num2;
+
+            }
+
+            Console.Write(num1 + calulation + num2 + "=" + resalt);
+
+            Console.ReadLine();
+
+        }
+
+        static void Add(double num1, double num2)
+        {
+            Console.WriteLine("{0}", num1 + num2);
+        }
+
+
+        static void Divide(double num1, double num2)
+        {
+
+            Console.WriteLine("{0}", num1 / num2);
+
+
+        }
+
+        static void Multiply(double num1, double num2)
+        {
+
+            Console.WriteLine("{0}", num1 * num2);
+
+        }
+
+        static void Minus(double num1, double num2)
+        {
+
+            Console.WriteLine("{0}", num1 - num2);
+
+        }
+
+
+        public static void Bank()
+        {
+
+            Console.ForegroundColor = ConsoleColor.Green;
 
 
             int pin = 123;
@@ -32,10 +142,10 @@ namespace jacquesApp
             int age = int.Parse(Console.ReadLine());
 
 
-            Console.Write (firstname + " " + lastname+" ");
+            Console.Write(firstname + " " + lastname + " ");
 
             Console.WriteLine("Enter pin");
-            pin = int.Parse (Console.ReadLine());
+            pin = int.Parse(Console.ReadLine());
 
             if (pin == 123 && age == 17 && firstname == "Jacques" && lastname == "Rockell")
             {
@@ -45,7 +155,7 @@ namespace jacquesApp
                 Console.WriteLine("Enter withdraw ammount:");
                 decimal withdraw = decimal.Parse(Console.ReadLine());
 
-                Console.WriteLine(firstname + " " + lastname + " " + "Savings$" + savings+"-" + "$" + withdraw);
+                Console.WriteLine(firstname + " " + lastname + " " + "Savings$" + savings + "-" + "$" + withdraw);
 
                 savings = savings - withdraw;
 
@@ -58,12 +168,17 @@ namespace jacquesApp
 
             }
 
-                
+
 
 
             Console.ReadLine();
 
 
         }
+
+
+
     }
+
+
 }
